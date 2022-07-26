08484dec11c474e5a95ef7ba30715f0860d7aca6
Ana Luiza SIlva <ana.silva@vortal.biz>
2022-07-26 07:51:08 +0100

    added react routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <BrowserRouter>
+     <div className="App">
+     <Routes>
+     <Route path='/' element={<Home/>} />
+     </Routes> 
     </div>
+    </BrowserRouter>
+   
   );
 }
 
